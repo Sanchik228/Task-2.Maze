d85f4e99274adf585a203cf35d59c810d4342fd3
Sanchik228 <Oleksandra.Slastikhina@cs.khpi.edu.ua>
2025-06-27 12:22:18 +0300

    added overrun

--- Task #2.Maze/Task #2.Maze.cpp
+++ Task #2.Maze/Task #2.Maze.cpp
@@ -11,13 +11,22 @@ std::vector<std::vector<char>> maze;
 const int dx[] = { -2, 2, 0 ,0 };
 const int dy[] = { 0, 0, -2, 2 };
 
+bool isValid(int x, int y) {
+    return x > 0 && x < rows - 1 && y > 0 && y < cols - 1 && maze[x][y] == WALL;
+}
+
 void generateMaze(int x, int y) {
     maze[x][y] = ROAD;
     std::vector<int> dirs = { 0, 1, 2, 3 };
 
     std::shuffle(dirs.begin(), dirs.end(), std::default_random_engine(std::time(0)));
 
-    
+    for (int i = 0; i < dirs.size(); i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+
+    }
 }
 
 int main() {
