0232cca5da250ed80fb63b9d206051c8b64380dd
Sanchik228 <Oleksandra.Slastikhina@cs.khpi.edu.ua>
2025-06-27 11:53:25 +0300

    create vector maze

--- Task #2.Maze/Task #2.Maze.cpp
+++ Task #2.Maze/Task #2.Maze.cpp
@@ -1,6 +1,18 @@
 ﻿#include <iostream>
+#include <vector>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    int rows, cols;
+    std::cout << "Enter maze size (rows cols): ";
+    std::cin >> rows >> cols;
+
+    std::vector<std::vector<char>> maze(rows, std::vector<char>(cols, '#'));
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            std::cout << maze[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    return 0;
 }
