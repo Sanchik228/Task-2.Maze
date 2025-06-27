31229039771f84b93ea0d04fafdd2a88f66e10d2
Sanchik228 <Oleksandra.Slastikhina@cs.khpi.edu.ua>
2025-06-27 12:13:17 +0300

    add const 'ROAD' and 'WALL' now rows, cols and maze are global elemants

--- Task #2.Maze/Task #2.Maze.cpp
+++ Task #2.Maze/Task #2.Maze.cpp
@@ -1,12 +1,27 @@
 ﻿#include <iostream>
 #include <vector>
+#include <random>
+
+const char ROAD = ' ';
+const char WALL = '#';
+
+int rows, cols;
+std::vector<std::vector<char>> maze;
+
+void generateMaze(int x, int y) {
+    maze[x][y] = ROAD;
+    std::vector<int> dirs = { 0, 1, 2, 3 };
+
+    std::shuffle(dirs.begin(), dirs.end(), std::default_random_engine(std::time(0)));
+
+
+}
 
 int main() {
-    int rows, cols;
     std::cout << "Enter maze size (rows cols): ";
     std::cin >> rows >> cols;
 
-    std::vector<std::vector<char>> maze(rows, std::vector<char>(cols, '#'));
+    maze = std::vector<std::vector<char>>(rows, std::vector<char>(cols, WALL));
 
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
